92cd3d0bdf909b00ecb21e139d2a7b47fbbf2e41
Varun Murpani <82239802+varunmurpani@users.noreply.github.com>
2021-08-15 20:18:23 +0530

    Update user.js

--- src/models/user.js
+++ src/models/user.js
@@ -111,7 +111,7 @@ userSchema.methods.toJSON=function() {
 
 userSchema.methods.generateAuthToken = async function () {
   const user = this
-  const token = jwt.sign({ _id: user._id.toString() }, 'andrew')
+  const token = jwt.sign({ _id: user._id.toString() }, 'andrewisalegend')
 
   user.tokens = user.tokens.concat({ token })
   await user.save()
